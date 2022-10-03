f1cc456419de97ffbe28a32ec7d7a18684332c2d
InuzukaTomohiro <ec2-user@ip-172-31-16-22.ap-northeast-1.compute.internal>
2022-10-03 14:09:51 +0000

    [add]Task_7

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
-  has_many :liked_users, through: :favorites, source: :user
+
   has_many :book_comments, dependent: :destroy
   validates :title, presence: true
   validates :body, presence: true, length:{maximum:200}
@@ -9,8 +9,8 @@ class Book < ApplicationRecord
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
-  
+
+
 
   def self.looks(search, word)
     if search == "perfect_match"
